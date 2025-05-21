828955e4227be48cbe93f38d5e4e370dec0920e3
geljohn <111166264+geljohn@users.noreply.github.com>
2025-05-21 11:51:08 +0800

    asdf

--- wp-api-client/svelte.config.js
+++ wp-api-client/svelte.config.js
@@ -1,7 +1,16 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter()
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+kit: {
+    adapter: adapter(),
+    // ... truncated ...
   }
 };
+
+export default config;
